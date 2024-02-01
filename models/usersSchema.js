32f2c152e92e7ead152fd6458bc3574d73be17e6
unknown <rubikonspace@ukr.net>
2024-02-01 23:38:41 +0200

    Update usersSchema.js

--- models/usersSchema.js
+++ models/usersSchema.js
@@ -1,32 +1,39 @@
 import Joi from "joi";
 import { Schema, model } from "mongoose";
 
-const emailRegexp = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+const emailRegexp =
+  /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        match: emailRegexp,
-        unique: true,
-        required: true
+      type: String,
+      match: emailRegexp,
+      unique: true,
+      required: true,
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     token: {
-        type: String,
-        default: ""
+      type: String,
+      default: "",
     },
     avatarURL: {
-        type: String,
-        required: [true, 'avatarURL is required']
+      type: String,
+      required: [true, "avatarURL is required"],
     },
-    theme: enum[]
-}, { versionKey: false, timestamps: true })
-const User = model('user', userSchema)
+    theme: {
+      type: String,
+      enum: ["dark", "light", "violet"],
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+const User = model("user", userSchema);
 export default User;
