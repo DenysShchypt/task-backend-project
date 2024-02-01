7cf775e791d790bb7fad21d1eee2841ad50d4bab
DenysShchypt <denysshchyptsovit@gmail.com>
2024-02-01 18:28:35 +0200

    add schema User and Board

---
+++ models/index.js
@@ -0,0 +1,2 @@
+export { default as User } from "./usersSchema.js";
+export { default as Board } from "./boardSchema.js";
---
+++ models/usersSchema.js
@@ -0,0 +1,31 @@
+import Joi from "joi";
+import { Schema, model } from "mongoose";
+
+const emailRegexp = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        match: emailRegexp,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    token: {
+        type: String,
+        default: ""
+    },
+    avatarURL: {
+        type: String,
+        required: [true, 'avatarURL is required']
+    },
+}, { versionKey: false, timestamps: true })
+const User = model('user', userSchema)
+export default User;
