ced770a84bf23366052f1e7826bbd71816abc8b9
Olegmkv <129410354+Olegmkv@users.noreply.github.com>
2024-02-05 10:03:03 +0200

    add examination idBoard

--- controllers/columns/addColumn.js
+++ controllers/columns/addColumn.js
@@ -1,15 +1,24 @@
+import { isValidObjectId } from 'mongoose';
+import Board from '../../models/boardsSchema.js';
 import Column from "../../models/columnsSchema.js"
 import { HttpError } from "../../helpers/index.js"
 
 const addColumn = async (req, res) => {
     const { _id: owner } = req.user;
-  
-    // перевірка body.boardId ;  
-    // const haveBoard = Board.findOne({body.boardId})
-    // if (!haveBoard) {
-    //     throw HttpError(400, "id board not send")
-    // };
+ 
+    // перевірка boardId на валідність ід   
+    const { boardId } = req.body;
+    if (!isValidObjectId(boardId)) {
+        throw HttpError(404, `board id=${_id} not valid!`)
+    };
 
+    // перевірка boardId на належність користувачу 
+    const haveBoard = Board.findOne({_id: boardId, owner})
+    if (!haveBoard) {
+        throw HttpError(404, `board id=${_id} not found!`)
+    };
+
+    // додаємо колонку
     const add = await Column.create({ ...req.body, owner });
 
     res.status(201).json(add)
