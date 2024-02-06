248119b9abea01f03b633af98b763204a529644f
DenysShchypt <129331185+DenysShchypt@users.noreply.github.com>
2024-02-06 23:57:43 +0200

    Delete controllers/auth/index.js
    вже існує

--- controllers/auth/index.js
+++
@@ -1,7 +0,0 @@
-export { default as signup } from "./signup.js";
-export { default as signin } from "./signin.js";
-export { default as logout } from "./logout.js";
-export { default as updateToken } from "./updateToken.js";
-export { default as googleAuth } from "./googleAuth.js";
-export { default as googleRedirect } from "./googleRedirect.js";
-export { default as getCurrent } from "./getCurrent.js";
